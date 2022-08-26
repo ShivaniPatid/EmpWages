bc9dad6a284c79109b763e669bf31bd23eb0e8be
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-26 22:53:58 +0530

    [shivani] part time wage using switch case

---
+++ CalEmpWage/PartTimeWageSwitchCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace CalEmpWage
+{
+    internal class PartTimeWageSwitchCase
+    {
+        public static void CalculatePartTimeWages()
+        {
+            int empHrs = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+            Random random = new Random();
+            int empCheck=random.Next(1,3);
+            switch (empCheck)
+            {
+                case 1:
+                    empHrs = 8;
+                    Console.WriteLine("Employee is present for full time");
+                    break;
+                case 2:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is present for part time");
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is absent");
+                    break;
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee daily wage including the part time is " + empWage);
+        }
+    }
+}
--- CalEmpWage/Program.cs
+++ CalEmpWage/Program.cs
@@ -7,7 +7,7 @@ namespace CalEmpWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage\t 3. Part time wage");
+            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage\t 3. Part time wage\n 4. Part time wage using switch case");
             int num = Convert.ToInt32(Console.ReadLine());
             switch(num)
             {
@@ -19,6 +19,9 @@ namespace CalEmpWage
                     break;
                 case 3:
                     PartTimeWage.CalculatePartTimeWage();
+                    break;
+                case 4:
+                    PartTimeWageSwitchCase.CalculatePartTimeWages();
                     break;
                 default :
                     Console.WriteLine("Entered wrong number");
