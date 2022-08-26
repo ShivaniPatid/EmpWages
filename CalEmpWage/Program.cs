707cf4cbb6135f243706bc3ef61a33a60797bfeb
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-26 17:14:07 +0530

    [Shivani] UC2-Daily Wage

--- CalEmpWage/Program.cs
+++ CalEmpWage/Program.cs
@@ -7,12 +7,15 @@ namespace CalEmpWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Employe Present absent\t");
+            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage");
             int num = Convert.ToInt32(Console.ReadLine());
             switch(num)
             {
                 case 1:
                     EmployeeCheck.EmployeeAttendance();
+                    break;
+                case 2:
+                    DailyWage.CalculateDailyWage();
                     break;
                 default :
                     Console.WriteLine("Entered wrong number");
