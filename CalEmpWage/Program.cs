ef53618682632c601fdce6bb7c59a5f8fedc7bf8
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-26 23:09:47 +0530

    [shivani] UC5-Wage for month

--- CalEmpWage/Program.cs
+++ CalEmpWage/Program.cs
@@ -7,7 +7,7 @@ namespace CalEmpWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage\t 3. Part time wage\n 4. Part time wage using switch case");
+            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage\t 3. Part time wage\n 4. Part time wage using switch case\t 5. Calculate wage for month");
             int num = Convert.ToInt32(Console.ReadLine());
             switch(num)
             {
@@ -22,6 +22,9 @@ namespace CalEmpWage
                     break;
                 case 4:
                     PartTimeWageSwitchCase.CalculatePartTimeWages();
+                    break;
+                case 5:
+                    WageForMonth.CalculateWageForMonth();
                     break;
                 default :
                     Console.WriteLine("Entered wrong number");
---
+++ CalEmpWage/WageForMonth.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace CalEmpWage
+{
+    internal class WageForMonth
+    {
+        public static void CalculateWageForMonth()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+            for(int day = 1; day <= 20; day++)
+            {
+                Random random = new Random();
+                int empCheck=random.Next(1,3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs=8;
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("daily employee wage is {0} total employee wage {1}",empWage,totalEmpWage);
+            }
+            Console.WriteLine("Total employee wage is "+totalEmpWage);
+        }
+    }
+}
