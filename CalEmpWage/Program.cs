ed2b1035f8dcbfbecdb64157ac5641c4c82694bc
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-30 23:01:17 +0530

    [Shivani] UC6 - wage for month using while loop

--- CalEmpWage/Program.cs
+++ CalEmpWage/Program.cs
@@ -7,7 +7,7 @@ namespace CalEmpWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage\t 3. Part time wage\n 4. Part time wage using switch case\t 5. Calculate wage for month");
+            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage\t 3. Part time wage\n 4. Part time wage using switch case\t 5. Calculate wage for month\t 6. Calculate wage for month using while loop");
             int num = Convert.ToInt32(Console.ReadLine());
             switch(num)
             {
@@ -25,6 +25,9 @@ namespace CalEmpWage
                     break;
                 case 5:
                     WageForMonth.CalculateWageForMonth();
+                    break;
+                case 6:
+                    WageForMonthWhileLoop.CalculateWage();
                     break;
                 default :
                     Console.WriteLine("Entered wrong number");
